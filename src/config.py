2bd7e35e72d005ef21851db325c4d516bef4a365
giseladevicente <devicentegisela@gmail.com>
2024-07-02 19:25:58 -0300

    Configuración MYSQL

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Configuración de MySQL
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root123'  
+MYSQL_DB = 'tareas'  
+
+# Rutas
+UPLOADS_FOLDER = 'uploads'  # Carpeta para subir archivos
+
