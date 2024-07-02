c951ad6c815886581281215a013ed2969ab7d1d6
giseladevicente <devicentegisela@gmail.com>
2024-07-02 19:28:20 -0300

    Creación de app.py

---
+++ src/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import os
+
+from config import *
+
+app = Flask(__name__)
+mysql = MySQL(app)
+
+app.config.from_object('config') # importa todas las variables de configuración definidas en config.py a la aplicación Flask,
+
+@app.route('/')
+def index():
+    # Conexión con MySQL
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM tareas ORDER BY id ASC')
+    tareas = cur.fetchall()
+    cur.close()
+    return render_template('tareas/index.html', tareas=tareas)
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        descripcion = request.form['descripcion']
+        fecha_limite = request.form['fecha_limite']
+
+        # Conexión con MySQL
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO tareas (titulo, descripcion, fecha_limite) VALUES (%s, %s, %s)", (titulo, descripcion, fecha_limite))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('index'))
+    return render_template('tareas/create.html')
+
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM tareas WHERE id = %s", (id,))
+    tarea = cur.fetchone()
+
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        descripcion = request.form['descripcion']
+        fecha_limite = request.form['fecha_limite']
+
+        cur.execute("UPDATE tareas SET titulo=%s, descripcion=%s, fecha_limite=%s WHERE id=%s", (titulo, descripcion, fecha_limite, id))
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('index'))
+
+    cur.close()
+    return render_template('tareas/edit.html', tarea=tarea)
+
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM tareas WHERE id = %s", (id,))
+    mysql.connection.commit()
+    cur.close()
+
+    return redirect(url_for('index'))
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
