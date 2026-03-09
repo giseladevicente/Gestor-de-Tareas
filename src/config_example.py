add4b6f2c420e3863b8b7d3408a8943cdc5217f7
giseladevicente <devicentegisela@gmail.com>
2026-03-09 11:22:19 -0300

    Add config_example.py file

---
+++ src/config_example.py
@@ -0,0 +1,6 @@
+# Copiar este archivo como config.py y completar con tus datos de MySQL
+MYSQL_HOST = '127.0.0.1'
+MYSQL_USER = 'tu_usuario'
+MYSQL_PASSWORD = 'tu_contraseña'
+MYSQL_DB = 'tareas'
+MYSQL_PORT = 3306  # Ajustar según tu XAMPP o MySQL
